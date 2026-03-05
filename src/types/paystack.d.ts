033a03ce7b548ca1520304188cc3e42d2d414296
Abisentry <Abisentry3@gmail.com>
2026-03-05 12:06:54 +0100

    paystack

---
+++ src/types/paystack.d.ts
@@ -0,0 +1,16 @@
+declare module '@paystack/inline-js' {
+    export default class PaystackPop {
+        newTransaction(options: {
+            key: string;
+            email: string;
+            amount: number;
+            currency?: string;
+            ref?: string;
+            label?: string;
+            metadata?: any;
+            onSuccess?: (transaction: any) => void;
+            onCancel?: () => void;
+            onLoad?: () => void;
+        }): void;
+    }
+}
